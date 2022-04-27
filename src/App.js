08e72837712ddfdfb88089f029be7b19e4a1f33d
irhamhkm <irhamhqm@gmail.com>
2022-04-27 15:37:27 +0700

    manage files location

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import CarDetail from "./CarDetail";
 import CarList from "./CarList";
 import Layout from "./Layout";
+import Register from "./Register";
+import Login from './Login';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<CarList />}/>
+          <Route path="register" element={<Register />}/>
+          <Route path="login" element={<Login />}/>
           <Route path="detail/:id" element={<CarDetail />} />
           <Route path="*" element={<div>error 404: page not found</div>} />
         </Route>
